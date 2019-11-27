f92fe1509601f0a0431217604f578d3836edd755
gkatzioura <gkatzioura@gmail.com>
2019-11-27 20:45:38 +0000

    Added PubSub streamer test suite

---
+++ modules/pub-sub/src/test/java/org/apache/ignite/stream/pubsub/PubSubStreamerTestSuite.java
@@ -0,0 +1,12 @@
+package org.apache.ignite.stream.pubsub;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Pub/Sub streamer tests.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({PubSubStreamerSelfTest.class})
+public class PubSubStreamerTestSuite {
+}
