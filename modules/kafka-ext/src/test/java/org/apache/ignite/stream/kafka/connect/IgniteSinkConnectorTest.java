489bf78d71cb910a8b111cc9638e50e237e10167
samaitra <saikat.maitra@gmail.com>
2020-11-07 22:00:05 -0600

    IGNITE-13648 Tests failing for extensions modules

--- modules/kafka-ext/src/test/java/org/apache/ignite/stream/kafka/connect/IgniteSinkConnectorTest.java
+++ modules/kafka-ext/src/test/java/org/apache/ignite/stream/kafka/connect/IgniteSinkConnectorTest.java
@@ -61,7 +61,7 @@ import static org.easymock.EasyMock.mock;
  */
 public class IgniteSinkConnectorTest extends GridCommonAbstractTest {
     /** Number of input messages. */
-    private static final int EVENT_CNT = 10000;
+    private static final int EVENT_CNT = 10;
 
     /** Cache name. */
     private static final String CACHE_NAME = "testCache";
@@ -70,7 +70,7 @@ public class IgniteSinkConnectorTest extends GridCommonAbstractTest {
     private static final String[] TOPICS = {"sink-test1", "sink-test2"};
 
     /** Kafka partition. */
-    private static final int PARTITIONS = 3;
+    private static final int PARTITIONS = 1;
 
     /** Kafka replication factor. */
     private static final int REPLICATION_FACTOR = 1;
@@ -205,7 +205,7 @@ public class IgniteSinkConnectorTest extends GridCommonAbstractTest {
             keyValMap.putAll(produceStream(topic, keyless));
 
         // Checks all events successfully processed in 10 seconds.
-        assertTrue(latch.await(60, TimeUnit.SECONDS));
+        assertTrue(latch.await(10, TimeUnit.SECONDS));
 
         grid.events(grid.cluster().forCacheNodes(CACHE_NAME)).stopLocalListen(putLsnr);
 
