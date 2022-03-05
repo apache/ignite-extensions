8d7c513eaf2600a1917300f8797661231e6d71c5
Nikolay <nizhikov@apache.org>
2022-03-05 18:10:43 +0300

    IGNITE-16664 Code review fixes.

--- modules/cdc-ext/src/main/java/org/apache/ignite/cdc/IgniteToIgniteCdcStreamer.java
+++ modules/cdc-ext/src/main/java/org/apache/ignite/cdc/IgniteToIgniteCdcStreamer.java
@@ -159,7 +159,7 @@ public class IgniteToIgniteCdcStreamer extends CdcEventsApplier implements CdcCo
     }
 
     /**
-     * Sets Ignite client node configuration thats will connect to destination cluster.
+     * Sets Ignite client node configuration that will connect to destination cluster.
      * @param destIgniteCfg Ignite client node configuration that will connect to destination cluster.
      * @return {@code this} for chaining.
      */
