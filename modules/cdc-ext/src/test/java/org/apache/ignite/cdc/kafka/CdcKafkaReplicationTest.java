717086d5fd14c77298ca5480b8d1896244bb0464
Nikolay Izhikov <nizhikov@apache.org>
2022-04-05 19:40:28 +0300

    IGNITE-14449 WIP

--- modules/cdc-ext/src/test/java/org/apache/ignite/cdc/kafka/CdcKafkaReplicationTest.java
+++ modules/cdc-ext/src/test/java/org/apache/ignite/cdc/kafka/CdcKafkaReplicationTest.java
@@ -218,6 +218,7 @@ public class CdcKafkaReplicationTest extends AbstractReplicationTest {
         cfg.setCaches(Collections.singletonList(cacheName));
         cfg.setTopic(topic);
         cfg.setMetadataTopic(metadataTopic);
+        cfg.setMetaConsumerGroup("meta-consumer-" + fromPart + "-" + toPart);
         cfg.setKafkaRequestTimeout(DFLT_KAFKA_REQ_TIMEOUT);
 
         return runAsync(new KafkaToIgniteCdcStreamer(igniteCfg, kafkaProperties(), cfg));
