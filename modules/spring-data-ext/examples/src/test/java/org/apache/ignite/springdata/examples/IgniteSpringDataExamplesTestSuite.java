065dcd0a9eb77676a72698e7387948e6c49e06c9
Ilya Shishkov <shishkovilja@gmail.com>
2022-06-09 19:19:32 +0300

    IGNITE-17053 Add test suite

---
+++ modules/spring-data-ext/examples/src/test/java/org/apache/ignite/springdata/examples/IgniteSpringDataExamplesTestSuite.java
@@ -0,0 +1,12 @@
+package org.apache.ignite.springdata.examples;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/** */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    SpringDataExampleTest.class
+})
+public class IgniteSpringDataExamplesTestSuite {
+}
