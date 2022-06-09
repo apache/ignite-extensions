0ead0ea38ec7f1855c45530f787b33c846842fe3
Ilya Shishkov <shishkovilja@gmail.com>
2022-06-09 16:47:14 +0300

    IGNITE-17053 Add test, change config dir path

---
+++ modules/spring-data-ext/examples/src/test/java/org/apache/ignite/springdata/examples/SpringDataExampleTest.java
@@ -0,0 +1,12 @@
+package org.apache.ignite.springdata.examples;
+
+import org.junit.Test;
+
+/** */
+public class SpringDataExampleTest {
+    /** */
+    @Test
+    public void testExample() {
+        SpringDataExample.main(new String[0]);
+    }
+}
