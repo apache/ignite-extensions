18e644402cd9eb97aa3b087d1633f59e637065af
Ilya Shishkov <shishkovilja@gmail.com>
2023-02-28 22:00:20 +0300

    IGNITE-18911 Inline javadoc in SpringTransactionExampleTest

--- modules/spring-tx-ext/examples/src/test/java/org/apache/ignite/transactions/spring/examples/SpringTransactionExampleTest.java
+++ modules/spring-tx-ext/examples/src/test/java/org/apache/ignite/transactions/spring/examples/SpringTransactionExampleTest.java
@@ -20,13 +20,9 @@ package org.apache.ignite.transactions.spring.examples;
 import org.apache.ignite.testframework.junits.common.GridAbstractExamplesTest;
 import org.junit.Test;
 
-/**
- *
- */
+/** */
 public class SpringTransactionExampleTest extends GridAbstractExamplesTest {
-    /**
-     *
-     */
+    /** */
     @Test
     public void testExample() {
         SpringTransactionExample.main(EMPTY_ARGS);
