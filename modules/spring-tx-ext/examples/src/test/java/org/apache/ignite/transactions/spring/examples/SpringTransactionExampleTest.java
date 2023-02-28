436eaa7d2e7ea0ad2eb01eb0bdba2e633cdf14c0
Ilya Shishkov <shishkovilja@gmail.com>
2023-02-28 13:08:51 +0300

    IGNITE-18911 Add SpringTransactionExampleTest

---
+++ modules/spring-tx-ext/examples/src/test/java/org/apache/ignite/transactions/spring/examples/SpringTransactionExampleTest.java
@@ -0,0 +1,17 @@
+package org.apache.ignite.transactions.spring.examples;
+
+import org.apache.ignite.testframework.junits.common.GridAbstractExamplesTest;
+import org.junit.Test;
+
+/**
+ *
+ */
+public class SpringTransactionExampleTest extends GridAbstractExamplesTest {
+    /**
+     *
+     */
+    @Test
+    public void testExample() {
+        SpringTransactionExample.main(EMPTY_ARGS);
+    }
+}
