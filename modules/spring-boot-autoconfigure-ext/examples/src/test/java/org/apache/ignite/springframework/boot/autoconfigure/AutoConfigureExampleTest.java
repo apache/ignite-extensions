2cddf6be02c2d049c2817dbee6a4c741460b31dc
Ilya Shishkov <shishkovilja@gmail.com>
2023-02-28 21:54:46 +0300

    IGNITE-18911 Add AutoConfigureExampleTest

---
+++ modules/spring-boot-autoconfigure-ext/examples/src/test/java/org/apache/ignite/springframework/boot/autoconfigure/AutoConfigureExampleTest.java
@@ -0,0 +1,13 @@
+package org.apache.ignite.springframework.boot.autoconfigure;
+
+import org.apache.ignite.testframework.junits.common.GridAbstractExamplesTest;
+import org.junit.Test;
+
+/** */
+public class AutoConfigureExampleTest extends GridAbstractExamplesTest {
+    /** */
+    @Test
+    public void testExample() {
+        AutoConfigureExample.main(EMPTY_ARGS);
+    }
+}
