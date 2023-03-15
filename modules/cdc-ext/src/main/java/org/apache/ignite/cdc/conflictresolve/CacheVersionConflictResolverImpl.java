fcfbd658c7b31ffe2e00b1a7c70ab07113dcfc67
nizhikov <nizhikov@apache.org>
2023-03-15 19:29:03 +0300

    IGNITE-17449 Support expireTime in replication

--- modules/cdc-ext/src/main/java/org/apache/ignite/cdc/conflictresolve/CacheVersionConflictResolverImpl.java
+++ modules/cdc-ext/src/main/java/org/apache/ignite/cdc/conflictresolve/CacheVersionConflictResolverImpl.java
@@ -111,7 +111,6 @@ public class CacheVersionConflictResolverImpl implements CacheVersionConflictRes
                     oldEntry.expireTime()
                 );
             }
-
         }
         else if (useNew)
             res.useNew();
