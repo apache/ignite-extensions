493535351a2766519a6004334739be7e7e44d227
Alexey Gidaspov <asgidaspov@sberbank.ru>
2024-01-16 14:21:11 +0300

    IGNITE-21261 refactoring

--- modules/cdc-ext/src/main/java/org/apache/ignite/cdc/kafka/KafkaToIgniteMetadataUpdater.java
+++ modules/cdc-ext/src/main/java/org/apache/ignite/cdc/kafka/KafkaToIgniteMetadataUpdater.java
@@ -107,7 +107,7 @@ public class KafkaToIgniteMetadataUpdater implements AutoCloseable, OffsetCommit
 
         List<PartitionInfo> topicMeta = cnsmr.partitionsFor(metaTopic, Duration.ofMillis(kafkaReqTimeout));
 
-        if (topicMeta.isEmpty())
+        if (F.isEmpty(topicMeta))
             throw new IgniteException("Unknown topic: " + metaTopic);
 
         parts = topicMeta
