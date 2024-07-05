651658e27b90793f1217310561cd98f708a1a79a
Maksim Davydov <MMDavydov@sberbank.ru>
2024-07-05 17:25:58 +0300

    IGNITE-22403 test refactored

--- modules/cdc-ext/src/test/java/org/apache/ignite/cdc/kafka/KafkaToIgnitePartitionDistributionTest.java
+++ modules/cdc-ext/src/test/java/org/apache/ignite/cdc/kafka/KafkaToIgnitePartitionDistributionTest.java
@@ -1,70 +1,43 @@
 package org.apache.ignite.cdc.kafka;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
+import org.apache.ignite.internal.util.typedef.F;
 import org.apache.ignite.lang.IgniteBiTuple;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
 import static org.apache.ignite.cdc.kafka.AbstractKafkaToIgniteCdcStreamer.kafkaPartitions;
 
 /** Tests the kafka topic partitions distribution over specified number of threads. */
-@RunWith(Parameterized.class)
 public class KafkaToIgnitePartitionDistributionTest extends GridCommonAbstractTest {
     /** Streamer config. */
-    private static KafkaToIgniteCdcStreamerConfiguration streamerCfg;
-
-    /** Thread count. */
-    @Parameterized.Parameter()
-    public Integer threadCnt;
-
-    /** Test result. */
-    @Parameterized.Parameter(1)
-    public List<IgniteBiTuple<Integer, Integer>> result;
-
-    /** @return Test parameters. */
-    @Parameterized.Parameters(name = "threadCnt={0}, result={1}")
-    public static Collection<?> parameters() {
-        List<Object[]> params = new ArrayList<>();
-
-        params.add(new Object[] {3, Arrays.asList(t(0, 3), t(3, 6), t(6, 8))});
-        params.add(new Object[] {4, Arrays.asList(t(0, 2), t(2, 4), t(4, 6), t(6, 8))});
-        params.add(new Object[] {5, Arrays.asList(t(0, 2), t(2, 4), t(4, 6), t(6, 7), t(7, 8))});
-
-        return params;
-    }
-
-    /**
-     * Shortcut method for test parameters' creation.
-     * @param l {@code int} starting partition.
-     * @param r {@code int} ending partition.
-     */
-    private static IgniteBiTuple<Integer, Integer> t(int l, int r) {
-        return new IgniteBiTuple<>(l, r);
-    }
-
-    /** {@inheritDoc} */
-    @Override protected void beforeTestsStarted() {
-        streamerCfg = new KafkaToIgniteCdcStreamerConfiguration();
-
-        streamerCfg.setKafkaPartsFrom(0);
-        streamerCfg.setKafkaPartsTo(8);
-    }
-
-    /** {@inheritDoc} */
-    @Override protected void beforeTest() throws Exception {
-        super.beforeTest();
-
-        streamerCfg.setThreadCount(threadCnt);
-    }
+    private final KafkaToIgniteCdcStreamerConfiguration streamerCfg = new KafkaToIgniteCdcStreamerConfiguration();
 
     /** Checks the topic partitions distribution over specified number of threads. */
     @Test
-    public void testActivePassiveReplication() {
+    public void testKafkaPartitions() {
+        testKafkaPartitionsCase(3, 0, 8, Arrays.asList(F.t(0, 3), F.t(3, 6), F.t(6, 8)));
+        testKafkaPartitionsCase(4, 0, 8, Arrays.asList(F.t(0, 2), F.t(2, 4), F.t(4, 6), F.t(6, 8)));
+        testKafkaPartitionsCase(5, 0, 8, Arrays.asList(F.t(0, 2), F.t(2, 4), F.t(4, 6), F.t(6, 7), F.t(7, 8)));
+
+        testKafkaPartitionsCase(3, 3, 11, Arrays.asList(F.t(3, 6), F.t(6, 9), F.t(9, 11)));
+        testKafkaPartitionsCase(4, 3, 11, Arrays.asList(F.t(3, 5), F.t(5, 7), F.t(7, 9), F.t(9, 11)));
+        testKafkaPartitionsCase(5, 3, 11, Arrays.asList(F.t(3, 5), F.t(5, 7), F.t(7, 9), F.t(9, 10), F.t(10, 11)));
+
+        testKafkaPartitionsCase(3, 1, 4, Arrays.asList(F.t(1, 2), F.t(2, 3), F.t(3, 4)));
+    }
+
+    /**
+     * Checks the topic partitions distribution over specified number of threads for specified parameters.
+     * @param threadCnt Thread count.
+     * @param result List of pairs defining partition ranges for each applier thread.
+     */
+    private void testKafkaPartitionsCase(int threadCnt, int partFrom, int partTo, List<IgniteBiTuple<Integer, Integer>> result) {
+        streamerCfg.setThreadCount(threadCnt);
+        streamerCfg.setKafkaPartsFrom(partFrom);
+        streamerCfg.setKafkaPartsTo(partTo);
+
         assertEquals(result, kafkaPartitions(streamerCfg));
     }
 }
