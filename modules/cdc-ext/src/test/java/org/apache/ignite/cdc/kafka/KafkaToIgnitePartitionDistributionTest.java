306d03a5b12b6ae9956ce15b297dfe4199a4dfdc
Maksim Davydov <MMDavydov@sberbank.ru>
2024-07-08 18:50:16 +0300

    IGNITE-22403 import fix

--- modules/cdc-ext/src/test/java/org/apache/ignite/cdc/kafka/KafkaToIgnitePartitionDistributionTest.java
+++ modules/cdc-ext/src/test/java/org/apache/ignite/cdc/kafka/KafkaToIgnitePartitionDistributionTest.java
@@ -19,12 +19,12 @@ package org.apache.ignite.cdc.kafka;
 
 import java.util.Arrays;
 import java.util.List;
-import org.apache.ignite.internal.util.typedef.F;
 import org.apache.ignite.lang.IgniteBiTuple;
 import org.apache.ignite.testframework.junits.common.GridCommonAbstractTest;
 import org.junit.Test;
 
 import static org.apache.ignite.cdc.kafka.AbstractKafkaToIgniteCdcStreamer.kafkaPartitions;
+import static org.apache.ignite.internal.util.lang.GridFunc.t;
 
 /** Tests Kafka partition ranges assignment to the multiple applier threads. */
 public class KafkaToIgnitePartitionDistributionTest extends GridCommonAbstractTest {
@@ -34,15 +34,15 @@ public class KafkaToIgnitePartitionDistributionTest extends GridCommonAbstractTe
     /** */
     @Test
     public void testKafkaPartitions() {
-        doTest(3, 0, 8, Arrays.asList(F.t(0, 3), F.t(3, 6), F.t(6, 8)));
-        doTest(4, 0, 8, Arrays.asList(F.t(0, 2), F.t(2, 4), F.t(4, 6), F.t(6, 8)));
-        doTest(5, 0, 8, Arrays.asList(F.t(0, 2), F.t(2, 4), F.t(4, 6), F.t(6, 7), F.t(7, 8)));
+        doTest(3, 0, 8, Arrays.asList(t(0, 3), t(3, 6), t(6, 8)));
+        doTest(4, 0, 8, Arrays.asList(t(0, 2), t(2, 4), t(4, 6), t(6, 8)));
+        doTest(5, 0, 8, Arrays.asList(t(0, 2), t(2, 4), t(4, 6), t(6, 7), t(7, 8)));
 
-        doTest(3, 3, 11, Arrays.asList(F.t(3, 6), F.t(6, 9), F.t(9, 11)));
-        doTest(4, 3, 11, Arrays.asList(F.t(3, 5), F.t(5, 7), F.t(7, 9), F.t(9, 11)));
-        doTest(5, 3, 11, Arrays.asList(F.t(3, 5), F.t(5, 7), F.t(7, 9), F.t(9, 10), F.t(10, 11)));
+        doTest(3, 3, 11, Arrays.asList(t(3, 6), t(6, 9), t(9, 11)));
+        doTest(4, 3, 11, Arrays.asList(t(3, 5), t(5, 7), t(7, 9), t(9, 11)));
+        doTest(5, 3, 11, Arrays.asList(t(3, 5), t(5, 7), t(7, 9), t(9, 10), t(10, 11)));
 
-        doTest(3, 1, 4, Arrays.asList(F.t(1, 2), F.t(2, 3), F.t(3, 4)));
+        doTest(3, 1, 4, Arrays.asList(t(1, 2), t(2, 3), t(3, 4)));
     }
 
     /**
