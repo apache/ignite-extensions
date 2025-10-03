6e31b35b25e37b6e1d1e06c0ee06922651277e60
Sergey Korotkov <serge.korotkov@gmail.com>
2025-10-03 17:04:06 +0700

    IGNITE-26510 Compilation fix (#321)

--- modules/cdc-ext/src/test/java/org/apache/ignite/cdc/kafka/KafkaToIgniteMetadataUpdaterTest.java
+++ modules/cdc-ext/src/test/java/org/apache/ignite/cdc/kafka/KafkaToIgniteMetadataUpdaterTest.java
@@ -156,7 +156,7 @@ public class KafkaToIgniteMetadataUpdaterTest extends GridCommonAbstractTest {
 
         GridTestUtils.setFieldValue(streamer, "log", listeningLog.getLogger(IgniteToKafkaCdcStreamer.class));
 
-        streamer.start(new MetricRegistryImpl("test", null, null, log));
+        streamer.start(new MetricRegistryImpl("test", null, null, null, log));
 
         return streamer;
     }
